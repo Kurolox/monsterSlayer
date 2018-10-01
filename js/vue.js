7d23289b73b0d2f3d62c09872953aed293829e7d
Carlos Gómez <kurolox@gmail.com>
2018-10-01 13:09:18 +0200

    Implemented health bar

--- js/vue.js
+++ js/vue.js
@@ -1,17 +1,33 @@
 new Vue({
     el: "#app",
     data: {
+        playerLife: 100,
+        monsterLife: 100,
 
     },
     methods: {
         attack: function(){
-            alert("test!");
-            console.log("asd");
+            this.monsterLife -= Math.floor(6 + Math.random() * 8);
+            if(this.monsterLife < 0){
+                this.monsterLife = 0;
+            }
+            console.log("Monster life: " + this.monsterLife);
+
+            if(this.monsterLife === 0){
+                // TODO: Call "You Win" function
+                
+            }
+
         },
 
     },
     computed: {
-
+        monsterBarStyle: function(){return {
+            'background-color': "green",
+            margin: 0,
+            color: "white",
+            width: this.monsterLife + '%',
+        }},
     },
     watch: {
         
