b64e81e9a9c4487b825ce025fbb595b2b43c6786
Carlos Gómez <kurolox@gmail.com>
2018-10-02 10:12:30 +0200

    Implemented logging for player attacks

--- js/vue.js
+++ js/vue.js
@@ -1,23 +1,30 @@
 new Vue({
     el: "#app",
     data: {
+        isGameStarted: false,
         playerLife: 100,
         monsterLife: 100,
-
+        log: [],
     },
     methods: {
-        attack: function(){
-            this.monsterLife -= Math.floor(6 + Math.random() * 8);
+        playerAttack: function(){
+            damageDealt = Math.floor(6 + Math.random() * 8); // Deals random damage between 6 and 13
+            this.monsterLife -= damageDealt;
             if(this.monsterLife < 0){
                 this.monsterLife = 0;
             }
             console.log("Monster life: " + this.monsterLife);
 
-            if(this.monsterLife === 0){
+            this.log.push({player: "The player deals " + damageDealt + " damage to the monster."});
+
+            if(this.monsterLife === 0){ 
                 // TODO: Call "You Win" function
                 
             }
+        },
 
+        startGame: function(){
+            this.isGameStarted = true;
         },
 
     },
@@ -27,6 +34,13 @@ new Vue({
             margin: 0,
             color: "white",
             width: this.monsterLife + '%',
+        }},
+
+        playerBarStyle: function(){return {
+            'background-color': "green",
+            margin: 0,
+            color: "white",
+            width: this.playerLife + '%',
         }},
     },
     watch: {
